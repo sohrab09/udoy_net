80f63268dfde1f0e8fcb038a730018cca9dc03da
sohrab09 <sohrab.cse9@gmail.com>
2025-01-16 13:34:33 +0600

    Change the main.dart file ui

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,58 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
-  final List<Widget> _pages = const [
-    HomeScreen(),
-    ScanScreen(),
-    DiscoverScreen(),
-  ];
+  final List<Widget> _pages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _pages.addAll([
+      HomeScreen(),
+      ScanScreen(),
+      DiscoverScreen(),
+    ]);
+  }
+
+  void handleSubmitData(String data) {
+    // Pass the data to the server
+    print('Data submitted: $data');
+    // Add your server submission logic here
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("UDOY Internet"),
+        centerTitle: true,
+        title: const Text(
+          "UDOY Internet",
+          style: TextStyle(
+              fontSize: 24,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              letterSpacing: 1.5,
+              wordSpacing: 2,
+              shadows: [
+                Shadow(
+                  offset: Offset(2, 2),
+                  blurRadius: 3,
+                ),
+              ]),
+        ),
+        titleSpacing: 0,
+        toolbarHeight: 60,
+        backgroundColor: Color(0xFF65AA4B),
+        elevation: 10,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.send, color: Colors.white, size: 30),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              handleSubmitData('');
+            },
+          ),
+        ],
       ),
       body: IndexedStack(
         index: _currentIndex,
